5fcd3799ce5b8ec0046cf1b3f97475867a449583
vladscript <melezio@gmail.com>
2026-02-05 16:59:10 -0600

    updates acquisition AIMs Cams

--- matlab/AIMs/stopAIMs_2Cams_Recording.m
+++ matlab/AIMs/stopAIMs_2Cams_Recording.m
@@ -1,6 +1,6 @@
 fprintf('\nClearing:')
-closepreview(v_CYL);
-closepreview(v_OF);
-delete(v_CYL); delete(v_OF);
-clear v_CYL v_OF
+closepreview(v_FRONT);
+closepreview(v_BOTTOM);
+delete(v_BOTTOM); delete(v_FRONT);
+clear v_BOTTOM v_FRONT
 fprintf('[COMPLETE]\n')
